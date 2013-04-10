ed117d3eaa8dc26fa352b9b11feea61d901a428d
Benjamin Rapaport <benjamin.rapaport@gmail.com>
2013-04-10 17:20:42 -0400

    adding better docstring for sigmrnd

--- util/sigmrnd.m
+++ util/sigmrnd.m
@@ -1,4 +1,8 @@
 function X = sigmrnd(P)
-%     X = double(1./(1+exp(-P)))+1*randn(size(P));
+%   P: vector
+%   This takes a vector, puts it through an element sigmoid function and
+%   samples a random binary variable from the probability binomial
+%   distribution defined by the sigmoid output
+%     X = double(1./(1+exp(-P)))+1 > rand(size(P));
     X = double(1./(1+exp(-P)) > rand(size(P)));
 end
