d093c8833c893f2ad9f3dff738762b6d9adb011e
Benjamin Rapaport <benjamin.rapaport@gmail.com>
2013-04-12 22:28:54 -0400

    randomly initializing weights from a gaussian, instead of starting with zeros

--- DBN/dbnsetup.m
+++ DBN/dbnsetup.m
@@ -1,4 +1,6 @@
 function dbn = dbnsetup(dbn, x, opts)
+    rand_weight_sigma = 0.01;
+    
     n = size(x, 2);
     dbn.sizes = [n, dbn.sizes];
 
@@ -6,15 +8,17 @@ function dbn = dbnsetup(dbn, x, opts)
         dbn.rbm{u}.alpha    = opts.alpha;
         dbn.rbm{u}.momentum = opts.momentum;
 
-        dbn.rbm{u}.W  = zeros(dbn.sizes(u + 1), dbn.sizes(u));
+        dbn.rbm{u}.W  = mvnrnd(zeros(dbn.sizes(u + 1), dbn.sizes(u)),...
+            rand_weight_sigma * eye(dbn.sizes(u)));
         % vW holds the delta for each weight update, for use with momentum
         % calculations
         dbn.rbm{u}.vW = zeros(dbn.sizes(u + 1), dbn.sizes(u));
 
-        dbn.rbm{u}.b  = zeros(dbn.sizes(u), 1);
+        dbn.rbm{u}.b  = mvnrnd(zeros(dbn.sizes(u), 1), rand_weight_sigma);
         dbn.rbm{u}.vb = zeros(dbn.sizes(u), 1);
 
-        dbn.rbm{u}.c  = zeros(dbn.sizes(u + 1), 1);
+        dbn.rbm{u}.c  = mvnrnd(zeros(dbn.sizes(u + 1), 1),...
+            rand_weight_sigma);
         dbn.rbm{u}.vc = zeros(dbn.sizes(u + 1), 1);
     end
 
