0cc58ab56024b70193dd538af9e953fd49b7b3a8
Benjamin Rapaport <benjamin.rapaport@gmail.com>
2013-04-17 15:22:55 -0400

    allowing nnpredict to pass nn back so we can get the activations from it

--- NN/nnpredict.m
+++ NN/nnpredict.m
@@ -1,8 +1,11 @@
-function labels = nnpredict(nn, x)
+% if opt_nn is specified, the activations of the input will remain 
+% in the returned nn.
+function [labels opt_nn] = nnpredict(nn, x)
     nn.testing = 1;
     nn = nnff(nn, x, zeros(size(x,1), nn.size(end)));
     nn.testing = 0;
     
     [~, i] = max(nn.a{end},[],2);
     labels = i;
+    opt_nn = nn;
 end
